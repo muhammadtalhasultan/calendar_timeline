acaeac7a23cc477b83ac2bc6caea483c57cf7278
alfredo-codigobase <alfredo@codigobase.es>
2020-04-27 14:27:18 +0200

    flutter format and year design

--- lib/calendar_timeline.dart
+++ lib/calendar_timeline.dart
@@ -17,28 +17,33 @@ class CalendarTimeline extends StatefulWidget {
   final Color monthColor;
   final Color dotsColor;
 
-  CalendarTimeline({Key key,
-    @required this.initialDate,
-    @required this.firstDate,
-    @required this.lastDate,
-    @required this.onDateSelected,
-    this.selectableDayPredicate,
-    this.leftMargin = 0,
-    this.dayColor,
-    this.activeDayColor,
-    this.activeBackgroundDayColor,
-    this.monthColor,
-    this.dotsColor
-  })
-    : assert(initialDate != null),
-      assert(firstDate != null),
-      assert(lastDate != null),
-      assert(!initialDate.isBefore(firstDate), 'initialDate must be on or after firstDate'),
-      assert(!initialDate.isAfter(lastDate), 'initialDate must be on or before lastDate'),
-      assert(!firstDate.isAfter(lastDate), 'lastDate must be on or after firstDate'),
-      assert(selectableDayPredicate == null || selectableDayPredicate(initialDate),
-      'Provided initialDate must satisfy provided selectableDayPredicate'),
-      super(key: key);
+  CalendarTimeline(
+      {Key key,
+      @required this.initialDate,
+      @required this.firstDate,
+      @required this.lastDate,
+      @required this.onDateSelected,
+      this.selectableDayPredicate,
+      this.leftMargin = 0,
+      this.dayColor,
+      this.activeDayColor,
+      this.activeBackgroundDayColor,
+      this.monthColor,
+      this.dotsColor})
+      : assert(initialDate != null),
+        assert(firstDate != null),
+        assert(lastDate != null),
+        assert(initialDate.difference(firstDate).inDays >= 0,
+            'initialDate must be on or after firstDate'),
+        assert(!initialDate.isAfter(lastDate),
+            'initialDate must be on or before lastDate'),
+        assert(!firstDate.isAfter(lastDate),
+            'lastDate must be on or after firstDate'),
+        assert(
+            selectableDayPredicate == null ||
+                selectableDayPredicate(initialDate),
+            'Provided initialDate must satisfy provided selectableDayPredicate'),
+        super(key: key);
 
   @override
   _CalendarTimelineState createState() => _CalendarTimelineState();
@@ -88,18 +93,20 @@ class _CalendarTimelineState extends State<CalendarTimeline> {
         itemExtent: _dayItemExtend,
         padding: EdgeInsets.only(left: widget.leftMargin, right: 10),
         itemBuilder: (BuildContext context, int index) {
-
           final currentDay = _days[index];
 
           return _DayItem(
             isSelected: _daySelectedIndex == index,
             dayNumber: _days[index].day,
-            shortName: _formatterDay.format(currentDay).capitalize().substring(0, 2),
+            shortName:
+                _formatterDay.format(currentDay).capitalize().substring(0, 2),
             textColor: widget.dayColor,
             activeTextColor: widget.activeDayColor,
             activeBackgroundColor: widget.activeBackgroundDayColor,
             onTap: () => _goToActualDay(index),
-            available: widget.selectableDayPredicate == null ? true : widget.selectableDayPredicate(currentDay),
+            available: widget.selectableDayPredicate == null
+                ? true
+                : widget.selectableDayPredicate(currentDay),
             dotsColor: widget.dotsColor,
           );
         },
@@ -127,15 +134,25 @@ class _CalendarTimelineState extends State<CalendarTimeline> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                if (widget.firstDate.year != currentDate.year && currentDate.month == 1)
+                if (widget.firstDate.year != currentDate.year &&
+                    currentDate.month == 1)
                   Padding(
                     padding: const EdgeInsets.only(right: 10),
-                    child: Text(
-                      DateFormat.y().format(currentDate),
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        fontSize: 20,
-                        color: widget.monthColor,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border:
+                              Border.all(color: widget.monthColor, width: 1),
+                          borderRadius: BorderRadius.circular(4)),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12.0, vertical: 5.0),
+                        child: Text(
+                          DateFormat.y().format(currentDate),
+                          style: TextStyle(
+                            color: widget.monthColor,
+                            fontSize: 12,
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -157,8 +174,9 @@ class _CalendarTimelineState extends State<CalendarTimeline> {
     _days.clear();
     for (var i = 1; i <= 31; i++) {
       final day = DateTime(selectedMonth.year, selectedMonth.month, i);
-      if (day.isBefore(widget.firstDate)) continue;
-      if (day.month != selectedMonth.month || day.isAfter(widget.lastDate)) break;
+      if (day.difference(widget.firstDate).inDays <= -1) continue;
+      if (day.month != selectedMonth.month || day.isAfter(widget.lastDate))
+        break;
       _days.add(day);
     }
   }
@@ -174,7 +192,8 @@ class _CalendarTimelineState extends State<CalendarTimeline> {
   _resetCalendar(DateTime date) {
     _generateDays(date);
     _daySelectedIndex = null;
-    _controllerDay.animateTo(0, duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+    _controllerDay.animateTo(0,
+        duration: Duration(milliseconds: 500), curve: Curves.easeIn);
   }
 
   _goToActualMonth(int index) {
@@ -211,15 +230,15 @@ class _CalendarTimelineState extends State<CalendarTimeline> {
   _initCalendar() {
     _generateMonths();
     _generateDays(widget.initialDate);
-    _monthSelectedIndex = _months.indexOf(
-      _months.firstWhere((monthDate) => monthDate.year == widget.initialDate.year
-        && monthDate.month == widget.initialDate.month)
-    );
-    _daySelectedIndex = _days.indexOf(
-      _days.firstWhere((dayDate) => dayDate.day == widget.initialDate.day)
-    );
+    _monthSelectedIndex = _months.indexOf(_months.firstWhere((monthDate) =>
+        monthDate.year == widget.initialDate.year &&
+        monthDate.month == widget.initialDate.month));
+    _daySelectedIndex = _days.indexOf(_days.firstWhere(
+      (dayDate) => dayDate.day == widget.initialDate.day,
+      orElse: () => _days[0],
+    ));
     _controllerDay = ScrollController(
-      initialScrollOffset:_daySelectedIndex * _dayItemExtend,
+      initialScrollOffset: _daySelectedIndex * _dayItemExtend,
     );
   }
 }
@@ -259,18 +278,18 @@ class _DayItem extends StatelessWidget {
   final bool available;
   final Color dotsColor;
 
-  const _DayItem({Key key,
-    @required this.dayNumber,
-    @required this.shortName,
-    @required this.isSelected,
-    @required this.onTap,
-    this.textColor,
-    this.activeTextColor,
-    this.activeBackgroundColor,
-    this.available = true,
-    this.dotsColor
-  })
-    : super(key: key);
+  const _DayItem(
+      {Key key,
+      @required this.dayNumber,
+      @required this.shortName,
+      @required this.isSelected,
+      @required this.onTap,
+      this.textColor,
+      this.activeTextColor,
+      this.activeBackgroundColor,
+      this.available = true,
+      this.dotsColor})
+      : super(key: key);
 
   final double height = 70.0;
   final double width = 60.0;
@@ -311,7 +330,6 @@ class _DayItem extends StatelessWidget {
   }
 
   Widget _buildDots() {
-    
     final dot = Container(
       height: 5,
       width: 5,
@@ -340,8 +358,9 @@ class _DayItem extends StatelessWidget {
               dayNumber.toString(),
               style: TextStyle(
                   color: available
-                    ? textColor ?? Theme.of(context).accentColor
-                    : textColor?.withOpacity(0.5) ?? Theme.of(context).accentColor.withOpacity(0.5),
+                      ? textColor ?? Theme.of(context).accentColor
+                      : textColor?.withOpacity(0.5) ??
+                          Theme.of(context).accentColor.withOpacity(0.5),
                   fontSize: 32,
                   fontWeight: FontWeight.normal),
             ),
