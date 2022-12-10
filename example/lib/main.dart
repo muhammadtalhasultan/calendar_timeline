a792fe093637bd5a1c13823eb5efa1716f6a5a26
Pablo Jiménez Pascual <pablo@jimpas.me>
2022-12-10 19:16:26 +0100

    Add basic lints to the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -34,13 +34,13 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _resetSelectedDate() {
-    _selectedDate = DateTime.now().add(Duration(days: 2));
+    _selectedDate = DateTime.now().add(const Duration(days: 2));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF333A47),
+      backgroundColor: const Color(0xFF333A47),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -59,37 +59,37 @@ class _HomePageState extends State<HomePage> {
               showYears: true,
               initialDate: _selectedDate,
               firstDate: DateTime.now(),
-              lastDate: DateTime.now().add(Duration(days: 365 * 4)),
+              lastDate: DateTime.now().add(const Duration(days: 365 * 4)),
               onDateSelected: (date) => setState(() => _selectedDate = date),
               leftMargin: 20,
               monthColor: Colors.white70,
               dayColor: Colors.teal[200],
-              dayNameColor: Color(0xFF333A47),
+              dayNameColor: const Color(0xFF333A47),
               activeDayColor: Colors.white,
               activeBackgroundDayColor: Colors.redAccent[100],
-              dotsColor: Color(0xFF333A47),
+              dotsColor: const Color(0xFF333A47),
               selectableDayPredicate: (date) => date.day != 23,
               locale: 'en',
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.only(left: 16),
               child: TextButton(
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(Colors.teal[200]),
                 ),
-                child: Text(
+                child: const Text(
                   'RESET',
                   style: TextStyle(color: Color(0xFF333A47)),
                 ),
                 onPressed: () => setState(() => _resetSelectedDate()),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Center(
               child: Text(
                 'Selected date is $_selectedDate',
-                style: TextStyle(color: Colors.white),
+                style: const TextStyle(color: Colors.white),
               ),
             )
           ],
